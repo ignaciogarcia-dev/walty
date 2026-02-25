18ba71e2ba009568705be59b7c249f00917b192d
ignaciogarcia-dev <ignaciogarcia.code@gmail.com>
2026-02-24 23:54:27 -0300

    Add SQL schema for addresses and users tables in drizzle migration file

---
+++ drizzle/0000_complex_carmella_unuscione.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "addresses" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"address" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"password_hash" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
