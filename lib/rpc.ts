366acf9d041ba231af51688c43ab7310465a0171
ignaciogarcia-dev <ignaciogarcia.code@gmail.com>
2026-02-26 20:27:03 -0300

    Remove unused RPC URLs from the RPC configuration
    - Eliminated the Ankr and public node RPC URLs from the RPCS array in the rpc.ts file.
    - This change reduces clutter and focuses on the primary RPC URL defined by the environment variable.
    
    These modifications enhance the clarity and maintainability of the RPC configuration.

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -2,8 +2,6 @@ import { http } from "viem"
 
 const RPCS = [
   process.env.NEXT_PUBLIC_RPC_URL!,
-  "https://rpc.ankr.com/eth_sepolia",
-  "https://ethereum-sepolia.publicnode.com",
 ]
 
 export function getTransport() {
