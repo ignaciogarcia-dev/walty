77846e905f9c439dbc8bdd7a7b77fee0bd1f1170
ignaciogarcia-dev <ignaciogarcia.code@gmail.com>
2026-03-04 00:02:41 -0300

    chore: update RPC URLs from Sepolia to Ethereum mainnet

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -3,8 +3,8 @@ import { http, fallback } from "viem"
 export function getTransport() {
   const rpcs = [
     process.env.NEXT_PUBLIC_RPC_URL,
-    "https://rpc.ankr.com/eth_sepolia",
-    "https://ethereum-sepolia.publicnode.com",
+    "https://rpc.ankr.com/eth",
+    "https://ethereum.publicnode.com",
   ].filter(Boolean) as string[]
 
   return fallback(rpcs.map((url) => http(url)))
